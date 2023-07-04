183b7dd79d1b9bd29f961271a8d5a809645cb8ba
Zac Cassini <zcassini@gmail.com>
2023-07-04 14:27:09 -0400

    fixed conversion tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -8,6 +8,7 @@ public class RewardValueTests {
     void create_with_cash_value() {
         double cashValue = 100;
         var rewardValue = new RewardValue(cashValue);
+
         assertEquals(cashValue, rewardValue.getCashValue());
     }
 
@@ -15,16 +16,25 @@ public class RewardValueTests {
     void create_with_miles_value() {
         int milesValue = 10000;
         var rewardValue = new RewardValue(milesValue);
+
         assertEquals(milesValue, rewardValue.getMilesValue());
-    }
+     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        int expectedMilesValue = (int) (cashValue / RewardValue.CASH_TO_MILES_CONVERSION_RATE);
+
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int miles = 10000;
+        var rewardValue = new RewardValue(miles);
+        double expectedCashValue = miles * RewardValue.MILES_TO_CASH_CONVERSION_RATE;
+
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
